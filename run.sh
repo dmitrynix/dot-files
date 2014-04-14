01ce60c8f1eb6ea9bd063ce7f96f90512669f528
Dmitry Nix <dmitry.nix@gmail.com>
2014-04-14 08:05:52 -0300

    Add zsh and zpreztorc

--- run.sh
+++ run.sh
@@ -5,6 +5,24 @@ set -e
 script_path=$(readlink -f "$0")
 directory_path=$(dirname "${script_path}")
 
+if [ ! -x "$(which git)" ]; then
+  echo "ERROR: git not installed"
+  exit 1
+fi
+
+prezto_path="${HOME}/.zprezto"
+echo " + ${prezto_path}"
+if [ -d "${prezto_path}" ]; then
+  cd ${prezto_path}
+  git pull --rebase origin master
+  git submodule update --init --recursive
+else
+  git clone --recursive \
+    https://github.com/sorin-ionescu/prezto.git ${prezto_path}
+  rm -fr ~/.zprofile
+  ln -s ${HOME}/.zprezto/runcoms/zprofile ~/.zprofile
+fi
+
 for file in ${directory_path}/dot/*
 do
   file_basename=$(basename $file)
