b9427fdb5ff74ca1b42b2ee0e2f92aac48a9c087
Dmitry Nix <dmitry.nix@gmail.com>
2014-08-26 21:11:09 -0300

    Remove scripts/binary.

--- scripts/binary.sh
+++
@@ -1,8 +0,0 @@
-echo " + local/Binary"
-binaries="${HOME}/local/Binary"
-if [ -d "${binaries}" ]; then
-  cd ${binaries} && git pull origin master
-else
-  mkdir -p "${HOME}/local"
-  git clone git://github.com/dmitrynix/Binary.git ${binaries}
-fi
