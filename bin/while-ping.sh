bc2a53ac0f88ff05fea248957d7f312318d8a301
Dmitry Nix <dmitry.nix@gmail.com>
2014-12-23 18:14:48 -0300

    Change while-ping.sh to accept optional ip address.

--- bin/while-ping.sh
+++ bin/while-ping.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-set -x
-#set -e
+if [ "${1}" == "" ]
+then
+  ip="8.8.8.8"
+else
+  ip=${1}
+fi
 
 while :
 do
-  ping -c 4 8.8.8.8
+  echo "# ping -c 4 ${ip}"
+  ping -c 4 ${ip}
+
+  echo "# sleep 5"
   sleep 5
 done
