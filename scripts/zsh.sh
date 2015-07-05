2874b2965239134ea66f9899af0a18783177db02
Dmitry Rocha <dmitryrck@gmail.com>
2015-07-05 11:44:41 -0300

    Fix scripts/zsh:
    ~ is not recognized as a directory, using HOME var instead.

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -2,7 +2,7 @@
 
 for plugin in zsh-syntax-highlighting zsh-history-substring-search
 do
-  if [ -d "~/.${plugin}" ]; then
+  if [ -d "${HOME}/.${plugin}" ]; then
     cd ~/.${plugin} && git pull origin master
   else
     git clone https://github.com/zsh-users/${plugin}.git \
